3538fe436ad4a007a61b255eb4bfc4dec6e75646
juan suarez <jsuarez@buk.co>
2025-01-13 11:50:26 -0500

    feat: agregamos modelos de notas y usuario

---
+++ server/app/models/note.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from app.models import Base
+
+class Note(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True, nullable=False)
+    content = Column(Text, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    owner_id = Column(Integer, ForeignKey("user.id"))
+    owner = relationship("User", back_populates="notes")
