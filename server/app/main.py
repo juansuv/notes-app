ed540165550181d71c3711f867f42fc439fa9506
juan suarez <jsuarez@buk.co>
2025-01-14 00:35:15 -0500

    feat: agrega websocket para compartir el estado de edicion de una nota

--- server/app/main.py
+++ server/app/main.py
@@ -3,11 +3,13 @@ from app.config.db import database
 import uvicorn
 from app.routers import user
 from app.routers import note  
-
+from app.routers import note_websocket
 app = FastAPI()
 
 app.include_router(user.router, prefix="/api/auth", tags=["Users"])
 app.include_router(note.router, prefix="/api/notes", tags=["Notes"])
+app.include_router(note_websocket.router, tags=["WebSockets"])
+
 
 @app.on_event("startup")
 async def startup():
